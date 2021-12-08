8e987cbddc13c5d86b37e50c3172c66be410300a
Daria K <ppartesia@gmail.com>
2021-12-08 21:03:49 +0700

    Update submissions deadline

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -16,9 +16,9 @@
           </div>
           <div class="logo-hackatom__short">Platinum - Starport</div>
           <div class="logo-hackatom__long tm-overline">
-            November 11 - December 8
+            November 11 - December 10
           </div>
-          <div class="logo-hackatom__short">Nov 11 - Dec 8</div>
+          <div class="logo-hackatom__short">Nov 11 - Dec 10</div>
         </div>
       </kinesis-element>
 
@@ -51,7 +51,7 @@
             date
           "
         >
-          7 PM Nov 11 – 7 PM Dec 08
+          7 PM Nov 11 – 7 PM Dec 10
         </div>
 
         <div class="mt-6 tm-text-center">
@@ -91,9 +91,9 @@ export default {
       moment,
       countdown: {
         now: Math.trunc(new Date(new Date().toUTCString()).getTime() / 1000),
-        date: '2021-12-08',
-        time: '19:00',
-        usage: moment.tz('2021-12-08 19:00', 'CET').format(),
+        date: '2021-12-10',
+        time: '18:00',
+        usage: moment.tz('2021-12-10 19:00', 'CET').format(),
         end: '2021-12-08T19:00:00Z',
       },
     }
